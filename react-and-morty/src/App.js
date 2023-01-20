7e7919989e4d9b96c8b21281e687605c6292b179
Stefan <stefan.tanase2121@gmail.com>
2023-01-20 11:09:39 +0200

    final commit

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardListComponent from "./components/CardListComponent/CardListComponent"
 import "./App.css";
 import { useCharacters, useLocations } from "./api/useData";
--- react-and-morty/src/components/CharacterComponent/CharacterComponent.jsx
+++ react-and-morty/src/components/CharacterComponent/CharacterComponent.jsx
@@ -1,8 +1,14 @@
 import CardComponent from 'react-bootstrap/Card';
+import { useEffect } from 'react';
 import './CharacterComponent.css'
-function CharacterComponent({character,onClick}) {
+function CharacterComponent({character,onClick,cursorY  }) {
+  const scroll = () => {
+    document.querySelector('.App').scrollTo({
+      top: 250
+    })
+  }
   return (
-    <CardComponent fluid="True" id="card" onClick={() => { onClick() }}>
+    <CardComponent fluid="True" id="card" onClick={() => { onClick() ; scroll() }}>
       <CardComponent.Img className='card-img' variant="left" src={character.image}/>
       <CardComponent.Body>
         <CardComponent.Title fluid="True">{character.name}</CardComponent.Title>
